2706f132ecaf91ae0d19631294149914805a856e
HaiPham <pthai076@icloud.com>
2023-01-29 18:52:17 +0700

    [mod] delete demo

--- shared/src/androidMain/kotlin/com/hkaito/edu/kmm_base/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.hkaito.edu.kmm_base
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/commonMain/kotlin/com/hkaito/edu/kmm_base/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.hkaito.edu.kmm_base
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
